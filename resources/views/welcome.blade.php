6cb4d12eadc6456c11e63d616c629597f4abfa9d
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-04 17:14:50 +0100

    ciclo foreach data_movie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,24 @@
 @dump($data_movies)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-Model-Controller</title>
+</head>
+<body>
+    <main>
+        @foreach ($data_movies as $valore)
+        <div class="film_cont">
+            <h2>Titolo: {{$valore['title']}}</h2>
+            <p>Titolo-Originale: {{$valore['original_title']}}</p>
+            <p>Nazionalità: {{$valore['nationality']}}</p>
+            <p>Data: {{$valore['date']}}</p>
+            <p>Voto: {{$valore['vote']}}</p>
+        </div>
+        @endforeach
+    </main>
+</body>
+</html>
+            
