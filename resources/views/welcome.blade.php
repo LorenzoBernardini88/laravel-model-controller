e2ff5d6abf441fc6f91eec2c45bb7e596e6b20c3
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-04 17:40:52 +0100

    mod scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,20 +5,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Laravel-Model-Controller</title>
 </head>
 <body>
     <main>
-        @foreach ($data_movies as $valore)
-        <div class="film_cont">
-            <h2>Titolo: {{$valore['title']}}</h2>
-            <p>Titolo-Originale: {{$valore['original_title']}}</p>
-            <p>Nazionalità: {{$valore['nationality']}}</p>
-            <p>Data: {{$valore['date']}}</p>
-            <p>Voto: {{$valore['vote']}}</p>
+        <div class="container">
+            @foreach ($data_movies as $valore)
+            <div class="film_cont">
+                <h2>Titolo: {{$valore['title']}}</h2>
+                <p>Titolo-Originale: {{$valore['original_title']}}</p>
+                <p>Nazionalità: {{$valore['nationality']}}</p>
+                <p>Data: {{$valore['date']}}</p>
+                <p>Voto: {{$valore['vote']}}</p>
+            </div>
+            @endforeach
         </div>
-        @endforeach
     </main>
 </body>
 </html>
-            
